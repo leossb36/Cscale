127c9aabb24219426f98fd6aa1880d97d02fb35c
Leonardo Barreiros <leossb36@gmail.com>
2022-10-07 10:28:13 -0300

    [#Fix]: Fix modules structure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,24 +1,11 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
-import { ConfigurationServer } from './config/configuration';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
-import { AppointmentResolver } from './module/appointment/appointment.resolver';
+import { AppointmentModule } from './module/appointment/appointment.module';
+import { CommonModule } from './module/common/common.module';
+import { UsersModule } from './module/user/user.module';
 
-const gqlImports = [AppointmentResolver];
+const gqlImports = [CommonModule, UsersModule, AppointmentModule];
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(new ConfigurationServer().get('mongo').url),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      playground: true,
-    }),
-    ...gqlImports,
-  ],
+  imports: [...gqlImports],
 })
 export class AppModule {}
